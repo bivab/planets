2ec58e0aee815c1b7bab725491c26c82f15ad0f6
David Schneider <david.schneider@bivab.de>
2017-10-06 10:37:25 +0200

    Added a test that checks for an update title in the data file, since pluto is only a dwarf planet

--- src/test/java/TestPluto.java
+++ src/test/java/TestPluto.java
@@ -6,4 +6,10 @@ public class TestPluto extends TestBase {
   public void testPluto() {
     Assert.assertTrue(this.lines.stream().anyMatch(line -> line.matches("Pluto")));
   }
+
+  @Test
+  public void testTitle() {
+    final String title = this.lines.get(0);
+    Assert.assertTrue(title.matches(".*and Dwarf Planets.*"));
+  }
 }
